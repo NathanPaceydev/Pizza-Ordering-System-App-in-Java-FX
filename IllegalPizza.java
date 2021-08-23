7cbf883c1b6d69024924a0f8638f1706f836c677
NathanPaceydev <64051575+NathanPaceydev@users.noreply.github.com>
2021-08-23 14:43:51 -0400

    Update IllegalPizza.java

--- IllegalPizza.java
+++ IllegalPizza.java
@@ -1,19 +1,10 @@
 package application;
 
 
-/**
- * Part of the sample solution for assignment 3. An exception class used by both Pizza and LineItem.
- * @author Alan McLeod
- * @version 1.0
- */
 public class IllegalPizza extends Exception {
 
 	private static final long serialVersionUID = 4593713417540203652L;
 
-	/**
-	 * The exception class' constructor.
-	 * @param message A message describing the error.
-	 */
 	public IllegalPizza (String message) {
 		super(message);
 	} // end constructor
